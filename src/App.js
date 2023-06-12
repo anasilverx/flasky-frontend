074aafddc574c868d25e619787994bfc42ddaec4
Ana Silvera <anasilverx@gmail.com>
2023-06-12 15:04:20 -0700

    moved Animal component to AnimalList component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Animal from './components/Animal';
+// import Animal from './components/Animal';
 import AnimalList from './components/AnimalList';
 
 function App() {
   return (
     <section>
       <h1>The Sapphire Animal Adoption Agency</h1>
-      <h2>Animal Listings</h2>
-      {/* I want a list animal: name + photo */}
+      
+      
       <AnimalList></AnimalList>
-      <Animal></Animal>
-      <Animal></Animal>
-      <Animal></Animal>
-      <Animal></Animal>
     </section>
   );
 }
--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './AnimalList.css';
+import Animal from './Animal';
 
 const AnimalList = () => {
     return (
         <section className='AnimalList'>
             <h2>Animal List</h2>
+            <Animal></Animal>
+            <Animal></Animal>
+            <Animal></Animal>
+            <Animal></Animal>
+            <Animal></Animal>
         </section>
     )
 };
