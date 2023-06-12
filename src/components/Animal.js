7463b91161c13fd826b448725c7d6a30b2584e22
Ana Silvera <anasilverx@gmail.com>
2023-06-12 14:55:16 -0700

    creates the Animal component to display one cat

---
+++ src/components/Animal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Animal.css';
+
+const Animal = () => {
+    
+
+    return (
+    <section className='Animal'>
+        <h3>Animal Name: Samson</h3>
+        <p> species: cat</p>
+        <img src='https://placekitten.com/g/200/300' alt="willow"></img>
+    </section>);
+};
+
+export default Animal;
